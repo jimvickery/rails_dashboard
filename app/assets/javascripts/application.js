2b53c8d85e59979e11bf34535f5b9f984624546b
Jim Vickery <jimrv8@yahoo.com>
2019-01-13 01:28:57 -0600

    added to applicatoin.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,27 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+//= require jquery/dist/jquery.min.js
+//= require popper.js/dist/umd/popper.js
+//= require bootstrap/dist/js/bootstrap.min.js
+//= require dom-factory/dist/dom-factory.js
+//= require material-design-kit/dist/material-design-kit.js
+$('.dropdown.notifications ul a.nav-link').click(function (e) {
+  e.stopPropagation();
+  $(this).tab('show');
+});
+// Self Initialize DOM Factory Components
+domFactory.handler.autoInit()
+// Connect button(s) to drawer(s)
+var sidebarToggle = Array.prototype.slice.call(document.querySelectorAll('[data-toggle="sidebar"]'))
+sidebarToggle.forEach(function (toggle) {
+  toggle.addEventListener('click', function (e) {
+    var selector = e.currentTarget.getAttribute('data-target') || '#default-drawer'
+    var drawer = document.querySelector(selector)
+    if (drawer) {
+      drawer.mdkDrawer.toggle()
+    }
+  })
+})
